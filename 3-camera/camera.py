5d1994bdc6ffa00c70bb6af41772465d9082edd0
Andrej Lucny <andylucny@gmail.com>
2024-06-07 19:59:11 +0200

    3

---
+++ 3-camera/camera.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2 as cv
+import time
+
+camera = cv.VideoCapture(0) 
+#camera = cv.VideoCapture(0,cv.CAP_DSHOW) 
+#camera = cv.VideoCapture("http://93.48.88.159/mjpg/video.mjpg")
+
+while True:
+    hasFrame, frame = camera.read()
+    if not hasFrame:
+        break
+    cv.imshow('camera',frame)
+    key = cv.waitKey(10)
+    if key == 27:
+        break
+    elif key == ord('s'): # save the image
+        name = str(round(time.time()))
+        cv.imwrite(name+'.png',frame)
+
+camera.release()
+cv.destroyAllWindows()
