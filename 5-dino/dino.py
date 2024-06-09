640d51582838444275b29d7905c9a2f6a4f7cf21
Andrej Lucny <andrej.lucny@me-inspection.sk>
2024-06-09 18:00:10 +0200

    5

--- 5-dino/dino.py
+++ 5-dino/dino.py
@@ -17,7 +17,7 @@ def download_DinoViT_model():
 
 download_DinoViT_model()
 
-session = ort.InferenceSession("dino_deits8-224-final.onnx", providers=['CUDAExecutionProvider'])
+session = ort.InferenceSession("dino_deits8-224-final.onnx", providers=['CUDAExecutionProvider','CPUExecutionProvider'])
 input_names = [input.name for input in session.get_inputs()] 
 output_names = [output.name for output in session.get_outputs()] 
 #print(input_names,'->',output_names)
